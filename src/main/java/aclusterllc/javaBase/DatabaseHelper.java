68213e44d45f5cf68254fa51e06e8d0a87b03123
Md. Shaiful Islam <islambuet@gmail.com>
2023-06-13 17:04:19 +0600

    Update DatabaseHelper.java

--- src/main/java/aclusterllc/javaBase/DatabaseHelper.java
+++ src/main/java/aclusterllc/javaBase/DatabaseHelper.java
@@ -137,20 +137,26 @@ public class DatabaseHelper {
         String query = String.format("SELECT machine_id,param_id,value FROM parameters WHERE machine_id=%d", machineId);
         return getSelectQueryResults(connection,query,new String[] { "machine_id", "param_id"});
     }
-    public static JSONArray getProductsHistory(Connection connection,int machineId,JSONObject params){
+    public static JSONObject getProductsHistory(Connection connection,int machineId,JSONObject params){
+        JSONObject resultJsonObject = new JSONObject();
         String query = "SELECT *,UNIX_TIMESTAMP(created_at) AS created_at_timestamp FROM products_history";
+        String totalQuery = "SELECT COUNT(id) as totalRecords FROM products_history";
 
         query+=String.format(" WHERE machine_id=%d",machineId);
+        totalQuery+=String.format(" WHERE machine_id=%d",machineId);
         if(params.has("to_timestamp")){
             query+=String.format(" AND UNIX_TIMESTAMP(created_at)<=%d",params.getInt("to_timestamp"));
+            totalQuery+=String.format(" AND UNIX_TIMESTAMP(created_at)<=%d",params.getInt("to_timestamp"));
         }
         if(params.has("from_timestamp")){
             query+=String.format(" AND UNIX_TIMESTAMP(created_at)>=%d",params.getInt("from_timestamp"));
+            totalQuery+=String.format(" AND UNIX_TIMESTAMP(created_at)>=%d",params.getInt("from_timestamp"));
         }
         if(params.has("reason")){
             int reason=params.getInt("reason");
             if(reason>-1){
                 query+=String.format(" AND reason=%d",params.getInt("reason"));
+                totalQuery+=String.format(" AND reason=%d",params.getInt("reason"));
             }
         }
         query+=" ORDER BY id DESC";
@@ -168,9 +174,16 @@ public class DatabaseHelper {
             }
         }
         query+=";";
-        return getSelectQueryResults(connection,query);
+        totalQuery+=";";
+        JSONArray totalQueryResult=getSelectQueryResults(connection,totalQuery);
+        resultJsonObject.put("totalRecords", totalQueryResult.getJSONObject(0).getInt("totalRecords"));
+        resultJsonObject.put("records", getSelectQueryResults(connection,query));
+        return resultJsonObject;
+
     }
-    public static JSONArray getStatisticsData(Connection connection,int machineId,String table,JSONObject params){
+
+    public static JSONObject getStatisticsData(Connection connection,int machineId,String table,JSONObject params){
+        JSONObject resultJsonObject = new JSONObject();
         String query = "SELECT *,UNIX_TIMESTAMP(created_at) AS created_at_timestamp FROM ";
         query+=table;
         query+=String.format(" WHERE machine_id=%d",machineId);
@@ -195,7 +208,8 @@ public class DatabaseHelper {
             }
         }
         query+=";";
-        return getSelectQueryResults(connection,query);
+        resultJsonObject.put("records", getSelectQueryResults(connection,query));
+        return resultJsonObject;
     }
 
 }
